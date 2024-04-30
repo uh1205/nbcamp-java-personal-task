c8472b30b43f8a1c463c46490314419d62e8026d
Robbie <robbie331@>
2024-04-30 18:12:58 +0900

    level 1 - 4

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class App {
@@ -7,37 +8,39 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        int firstNumber = Integer.parseInt(sc.nextLine());
+        do {
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int firstNumber = Integer.parseInt(sc.nextLine());
 
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        int secondNumber = Integer.parseInt(sc.nextLine());
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int secondNumber = Integer.parseInt(sc.nextLine());
 
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        char operator = sc.nextLine().charAt(0);
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            char operator = sc.nextLine().charAt(0);
 
-        double result;
+            double result;
 
-        if (operator == '+') {
-            result = firstNumber + secondNumber;
-        }
-        else if (operator == '-') {
-            result = firstNumber - secondNumber;
-        }
-        else if (operator == '*') {
-            result = firstNumber * secondNumber;
-        }
-        else if (operator == '/') {
-            if (secondNumber == 0) {
-                System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+            if (operator == '+') {
+                result = firstNumber + secondNumber;
+            } else if (operator == '-') {
+                result = firstNumber - secondNumber;
+            } else if (operator == '*') {
+                result = firstNumber * secondNumber;
+            } else if (operator == '/') {
+                if (secondNumber == 0) {
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                    return;
+                }
+                result = (double) firstNumber / secondNumber;
+            } else {
+                System.out.println("사칙연산 기호에 맞는 연산자를 사용해주세요.");
                 return;
             }
-            result = (double) firstNumber / secondNumber;
-        } else {
-            System.out.println("사칙연산 기호에 맞는 연산자를 사용해주세요.");
-            return;
-        }
 
-        System.out.println("결과 : " + result);
+            System.out.println("결과 : " + result);
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+
+        } while (!Objects.equals(sc.nextLine(), "exit"));
     }
 }
