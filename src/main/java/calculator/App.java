8c8ac7401cc20a71c39aa19670c5edf779354a72
Robbie <robbie331@>
2024-04-30 18:19:21 +0900

    level 1 - 5

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -8,6 +8,9 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        double[] result = new double[10]; // 연산 결과 10개를 저장할 수 있는 배열
+        int count = 0; // 연산의 결과가 저장된 배열의 마지막 index 를 저장하는 변수
+
         do {
             System.out.print("첫 번째 숫자를 입력하세요: ");
             int firstNumber = Integer.parseInt(sc.nextLine());
@@ -18,26 +21,24 @@ public class App {
             System.out.print("사칙연산 기호를 입력하세요: ");
             char operator = sc.nextLine().charAt(0);
 
-            double result;
-
             if (operator == '+') {
-                result = firstNumber + secondNumber;
+                result[count] = firstNumber + secondNumber;
             } else if (operator == '-') {
-                result = firstNumber - secondNumber;
+                result[count] = firstNumber - secondNumber;
             } else if (operator == '*') {
-                result = firstNumber * secondNumber;
+                result[count] = firstNumber * secondNumber;
             } else if (operator == '/') {
                 if (secondNumber == 0) {
                     System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
                     return;
                 }
-                result = (double) firstNumber / secondNumber;
+                result[count] = (double) firstNumber / secondNumber;
             } else {
                 System.out.println("사칙연산 기호에 맞는 연산자를 사용해주세요.");
                 return;
             }
 
-            System.out.println("결과 : " + result);
+            System.out.println("결과 : " + result[count++]);
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
 
