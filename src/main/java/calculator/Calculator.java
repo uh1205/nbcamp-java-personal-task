acf1fa9ff87bf811a827c1f61f781e712dfede07
Robbie <robbie331@>
2024-04-30 20:59:01 +0900

    level 2 - 1

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,5 +1,24 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Calculator {
 
+    List<Double> results = new ArrayList<>();
+
+    public double calculate(int firstNum, int secondNum, char operator) {
+        return switch (operator) {
+            case '+' -> firstNum + secondNum;
+            case '-' -> firstNum - secondNum;
+            case '*' -> firstNum * secondNum;
+            case '/' -> {
+                if (secondNum == 0) {
+                    throw new ArithmeticException("Division by zero");
+                }
+                yield (double) firstNum / secondNum;
+            }
+            default -> throw new IllegalArgumentException("Invalid operator: " + operator);
+        };
+    }
 }
