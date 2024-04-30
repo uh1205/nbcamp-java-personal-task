3adb71980709c1f790f9399e5d86a15cf319b408
Robbie <robbie331@>
2024-04-30 22:08:43 +0900

    level 2 - 7

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -6,33 +6,61 @@ public class App {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Calculator calc = new Calculator(new ArrayList<>());
+        Calculator calc = new Calculator(new ArrayList<>(), new ArrayList<>());
 
         do {
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            int firstNumber = Integer.parseInt(sc.nextLine());
-
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            int secondNumber = Integer.parseInt(sc.nextLine());
-
-            System.out.print("사칙연산 기호를 입력하세요: ");
-            char operator = sc.nextLine().charAt(0);
-
-            double result = calc.calculate(firstNumber, secondNumber, operator);
-            System.out.println("결과 : " + result); // 연산 결과 출력
-            calc.addResult(result); // 연산 결과 저장
-
-            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-            if (Objects.equals(sc.nextLine(), "remove")) {
-                calc.removeResult();
-            }
-
-            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-            if (Objects.equals(sc.nextLine(), "inquiry")) {
-                calc.inquiryResults();
+            System.out.print("사칙 연산을 진행하려면 1을, 원의 넓이를 구하려면 아무거나 입력하세요: ");
+            if (Objects.equals(sc.nextLine(), "1")) {
+                calculate(sc, calc);
+            } else {
+                calculateCircleArea(sc, calc);
             }
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
         } while (!Objects.equals(sc.nextLine(), "exit"));
+    }
+
+    private static void calculate(Scanner sc, Calculator calc) {
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int firstNumber = Integer.parseInt(sc.nextLine());
+
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int secondNumber = Integer.parseInt(sc.nextLine());
+
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        char operator = sc.nextLine().charAt(0);
+
+        double result = calc.calculate(firstNumber, secondNumber, operator); // 연산 수행
+        System.out.println("결과 : " + result); // 연산 결과 출력
+        calc.addResult(result); // 연산 결과 저장
+
+        System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+        if (Objects.equals(sc.nextLine(), "remove")) {
+            calc.removeResult();
+        }
+
+        System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+        if (Objects.equals(sc.nextLine(), "inquiry")) {
+            calc.inquiryResults();
+        }
+    }
+
+    private static void calculateCircleArea(Scanner sc, Calculator calc) {
+        System.out.print("반지름을 입력하세요: ");
+        int radius = Integer.parseInt(sc.nextLine());
+
+        double result = calc.calculateCircleArea(radius); // 연산 수행
+        System.out.println("결과 : " + result); // 연산 결과 출력
+        calc.addCircleResult(result); // 연산 결과 저장
+
+        System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+        if (Objects.equals(sc.nextLine(), "remove")) {
+            calc.removeCircleResult();
+        }
+
+        System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+        if (Objects.equals(sc.nextLine(), "inquiry")) {
+            calc.inquiryCircleResults();
+        }
     }
 }
